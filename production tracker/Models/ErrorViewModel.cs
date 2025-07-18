e2375ec51a8373ae56d862246711cf8e1a4d4dc1
sanjaysagarad05 <sanjaysagarad@gmail.com>
2025-07-18 11:32:59 +0530

    Add project files.

---
+++ production tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace production_tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
